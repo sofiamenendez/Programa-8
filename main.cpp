fe87e7a76af79eb9b4293e642aa26b7f454b0b5e
sofiamenendez <samenendez97@unitec.edu>
2016-02-20 16:28:32 -0600

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+/* "Ingresar la nota acumulada y examen, luego obtener la nota final, ajustarla a 60 si se encuentra
+ entre 57-59 a 60 , presentar la nota y el estado si es aprobado o reprobado.*/
+int main()
+int nf=na+ne;
+if ((nf>=57)and (nf<=59))
+{ nf=60;
+    cout << "Nota final es "<<nf<<"ln";
+        if (nf>=60
+{
+    cout <<"aprobado ln";
+}
+else
+{
+    cout<<"reprobado ln";
+}
+    return 0;
+}
